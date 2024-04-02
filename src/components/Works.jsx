fa2974ea6c7cd68b56b155e433a06c505839b846
Cherishmi <cherishmibiyeshkaxox@gmail.com>
2024-04-02 13:40:08 +1100

    changes to work.js

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { github, live } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
-import { fadeIn, textVariant } from "../utils/motion";
 
 const ProjectCard = ({
   index,
@@ -17,7 +15,7 @@ const ProjectCard = ({
   demo_link,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <div>
       <div className='bg-tertiary p-5 rounded-2xl sm:w-[500px] w-full'>
         <div className='relative w-full h-[230px]'>
           <img
@@ -64,25 +62,24 @@ const ProjectCard = ({
           ))}
         </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <div>
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-      </motion.div>
+      </div>
 
       <div className='w-full flex'>
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
+        <p
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
           Following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
-        </motion.p>
+        </p>
       </div>
 
       <div className='mt-20 flex flex-wrap gap-7'>
